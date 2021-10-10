33bc103267abc07b4ccb53cd2e5db5bbf7fc313f
lss1314dks <92236821+lss1314dks@users.noreply.github.com>
2021-10-10 10:06:49 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main()
 {
-	printf("hello word ");
+	printf("headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36 SLBrowser/7.0.0.6241 SLBChan/103'
+        } ");
 	return 0;
 }
